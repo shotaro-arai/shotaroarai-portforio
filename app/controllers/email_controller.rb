1eba8cdacf9ecf8d805af4ff6288d5c2d20163b1
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-05-21 15:48:14 +0900

    new、createアクションを実装

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,2 +1,22 @@
 class EmailController < ApplicationController
+  def new
+    @email = Email.new
+  end
+
+  def create
+    @email = Email.new(email_params)
+    if @email.valid?
+      @email.save
+      EmailMailer.send_mail(@email).deliver_now
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+    def email_params
+      params.require(:email).permit(:name, :email, :title, :text)
+    end
 end
