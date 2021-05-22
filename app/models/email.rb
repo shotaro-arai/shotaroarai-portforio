ec2d2c57448546061672939c2669ded4883cfc1f
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-05-22 14:35:31 +0900

    エラー文を日本語に変更

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,7 @@
 class Email < ApplicationRecord
   with_options presence: true do
-    validates :name 
-    validates :email, format:{with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i} 
+    validates :name
+    validates :email, format:{with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "が間違っています"} 
     validates :title 
     validates :text
   end
