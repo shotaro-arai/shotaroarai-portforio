eb969a6b1c9bbc9b8d1ecc4c3831560627ceedf9
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-05-22 14:24:23 +0900

    emailにバリデーションを追加し、エラー文が表示されるようにemail/new.html.erbを編集

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,8 @@
 class Email < ApplicationRecord
-  validates :name, :email, :title, :text, presence: true
+  with_options presence: true do
+    validates :name 
+    validates :email, format:{with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i} 
+    validates :title 
+    validates :text
+  end
 end
